d70d87b49216315cd69df7278883b3e8d9c59a4b
tpires1041 <tiagopires2501@outlook.com>
2023-12-01 12:38:41 -0400

    Outro teste
    Teste com o embed do youtube

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,3 +1,19 @@
 import React from "react";
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/NtgwsMP-AtQ?si=sVZuvJxMGHCSg9_N" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+function YoutubeEmbed(props) {
+    const { videoId } = props;
+  
+    return (
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    );
+  }
+  
+  export default YoutubeEmbed;
