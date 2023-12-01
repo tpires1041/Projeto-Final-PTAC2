16071f071f0779360d17322dd3d1d6b52c502b8a
tpires1041 <tiagopires2501@outlook.com>
2023-12-01 12:42:45 -0400

    Formulário
    Adicionado o input do video no formulario

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -1,3 +1,6 @@
 export default function Formulario(){
-    
+    <form>
+        <label for="nomeDoVideo">Nome do vídeo</label>
+        <input type="text" id="nomeDoVideo" name="nomeDoVideo"><br>
+    </form>
 }
